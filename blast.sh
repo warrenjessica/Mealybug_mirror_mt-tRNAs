d315e7038d40d50444bda6f00d419bd783e155b7
warrenjessica <54719134+warrenjessica@users.noreply.github.com>
2025-11-24 19:51:50 -0700

    Delete blast.sh

--- blast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-for file in *collapsed.ID.fasta;
-	do blastn -task blastn -dust no -query ${file} -db pcitri_db_update3.fas -out ${file%_collapsed.ID.fasta}.collapsed.ID.BLAST.txt
-	
-	
-done;
